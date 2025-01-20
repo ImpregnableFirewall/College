715a294245aeab335b41cadf229b39b5399f8318
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2025-01-20 15:41:58 +0530

    Update q9.java

--- sem4/OOS/A1/q9.java
+++ sem4/OOS/A1/q9.java
@@ -33,7 +33,7 @@ class Person
     }
     void read()
     {
-        System.out.println("Name: "+get_name()+"\nAge: "+get_age()+"\nWeight: "+get_weight()+"\nHeight: "+get_height()+" cm\nDOB: "+get_dob()+"\nAddress: "+get_address());
+        System.out.println("Age: "+get_age()+"\nWeight: "+get_weight()+"\nHeight: "+get_height()+" cm\nDOB: "+get_dob()+"\nAddress: "+get_address());
     }
 }
 
@@ -42,11 +42,11 @@ class Employee extends Person
     float sal;
     String doj;
     int exp;
-    class Employee(int a, int h, float w, String d, String ad,float s, String d,int e)
+    Employee(int a, int h, float w, String d, String ad,float s, String dj,int e)
     {
         super(a,h,w,d,ad);
         sal=s;
-        doj=d;
+        doj=dj;
         exp=e;
     }
     void read()
@@ -57,17 +57,47 @@ class Employee extends Person
 }
 class Technician extends Employee
 {
-    
+    Technician(int a, int h, float w, String d, String ad,float s, String dj,int e)
+    {
+        super(a,h,w,d,ad,s,dj,e);
+    }
+    void read()
+    {
+        super.read();
+        System.out.println("Technician ready");
+    }
 }
-class Professsor extends Employee
+class Professor extends Employee
 {
-    
+    String courses[]=new String[2];
+    String advisee[]=new String[3];
+    Professor(int a, int h, float w, String d, String ad,String c[],String adv[],float s, String dj,int e)
+    {
+        super(a,h,w,d,ad,s,dj,e);
+        courses=c;
+        advisee=adv;
+    }
+    void read()
+    {
+        super.read();
+        int i=0;
+        System.out.println("Courses teaching: ");
+        for(;i<=1;i++)
+        {
+            System.out.println(courses[i]);
+        }
+        System.out.println("Advisee list: ");
+        for(;i<=2;i++)
+        {
+            System.out.println(advisee[i]);
+        }
+    }
 }
 class Student extends Person
 {
     int roll;
     float subs[]=new float[5];
-    class Employee(int a, int h, float w, String d, String ad,float s[],int r)
+    Student(int a, int h, float w, String d, String ad,float s[],int r)
     {
         super(a,h,w,d,ad);
         subs=s;
@@ -76,6 +106,7 @@ class Student extends Person
     void calc()
     {
         float s=0;
+        int i=0;
         for(;i<=4;i++)
         {
             s+=subs[i];
@@ -98,11 +129,34 @@ class Student extends Person
         super.read();
         System.out.println("Roll no.: "+roll);
         int i=0;
-        System.out.println("Marks: ")
+        System.out.println("Marks: ");
         for(;i<=4;i++)
         {
             System.out.println("Sub"+(i+1)+": "+subs[i]);
         }
         calc();
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        float mar[]={100f,98f,99f,95f,70f};
+        Person s=new Student(21,180,71,"12/05/2004","Kol-93",mar,13);
+        System.out.println("\nStudent: ");
+        print(s);
+        String c[]={"Engg.","Science"};
+        String a[]={"Soham","Anushka","Ritam"};
+        Person p=new Professor(30,170,65,"12/05/1995","Barasat",c,a,10000f,"12/05/2018",8);
+        System.out.println("\nProfessor: ");
+        print(p);
+        Person t=new Technician(25,190,60,"12/05/2000","Durgapur",5000f,"12/05/2019",7);
+        System.out.println("\nTechnician: ");
+        print(t);
+    }
+    static void print(Person p)
+    {
+        p.read();
     }
 }
