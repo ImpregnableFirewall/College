22e40c798ae0f3aa5e85d312b02d4b028fe6f51c
ImpregnableFirewall <137094661+ImpregnableFirewall@users.noreply.github.com>
2025-01-26 19:48:16 +0530

    Update q14.java

--- sem4/OOS/A1/q14.java
+++ sem4/OOS/A1/q14.java
@@ -1,103 +1,108 @@
 interface Shape
 {
-        void area();
-        void draw();
-        void rotate();
-        void move();
+    void area();
+    void draw();
+    void rotate();
+    void move();
 }
 
 class Circle implements Shape
 {
-        Circle(int number)
+    float radius;
+    int n;
+    Circle(float r,int n)
 	{
-		System.out.println("---------------------------------------------");
-		System.out.println("Printing details of circle "+number+" : ");
+        radius=r;
+        this.n=n;
 	}
 
 	public void area()
-        {
-                System.out.println("Area of circle is PI * Radius * Radius");
-        }
+    {
+        System.out.println("Area of circle "+n+": "+ (3.14*radius*radius));
+    }    
 
-        public void draw()
-        {
-                System.out.println("Drawing a circle");
-        }
+    public void draw()
+    {
+        System.out.println("Drawing a circle");
 
-        public void rotate()
-        {
-                System.out.println("Rotating the circle");
-        }
+    }
 
-        public void move()
-        {
-                System.out.println("Moving the circle");
-        }
+    public void rotate()
+    {
+        System.out.println("Rotating the circle");
+    }
+
+    public void move()
+    {
+        System.out.println("Moving the circle");
+    }
 
 	void getDetails()
 	{
-		this.area();
-		this.draw();
-		this.rotate();
-		this.move();
+		area();
+		draw();
+		rotate();
+		move();
 	}
 }
 
 class Rectangle implements Shape
 {       
-	Rectangle(int number)
+    float len,bre;
+    int n;
+	Rectangle(float l, float b,int n)
 	{
-		System.out.println("---------------------------------------------");
-		System.out.println("Printing details of rectangle "+number+" : ");
+		len=l;
+        bre=b;
+        this.n=n;
 	}
 
-        public void area()
-        {
-                System.out.println("Area of rectangle is Length * Breadth");
-        }
+    public void area()
+    {
+        System.out.println("Area of rectangle "+n+" : "+ (len*bre));
+    }
 
-        public void draw()
-        {
-                System.out.println("Drawing a rectangle");
-        }
+    public void draw()
+    {
+        System.out.println("Drawing a rectangle");
+    }
 
-        public void rotate()
-        {
-                System.out.println("Rotating the rectangle");
-        }
+    public void rotate()
+    {
+        System.out.println("Rotating the rectangle");
+    }
 
-        public void move()
-        {
-                System.out.println("Moving the rectangle");
-        }
+    public void move()
+    {
+        System.out.println("Moving the rectangle");
+    }
 
 	void getDetails()
 	{
-		this.area();
-		this.draw();
-		this.rotate();
-		this.move();
+		area();
+		draw();
+		rotate();
+		move();
 	}
 }
 
-class Shapes
+class Main
 {
 	public static void main(String args[])
 	{
-		Circle c1 = new Circle(1);
+		Circle c1 = new Circle(5f,1);
 		c1.getDetails();
 
-		Circle c2 = new Circle(2);
+		Circle c2 = new Circle(7.6f,2);
 		c2.getDetails();
 
-		Rectangle r1 = new Rectangle(1);
+		Rectangle r1 = new Rectangle(3f,4f,1);
 		r1.getDetails();
 
-		Rectangle r2 = new Rectangle(2);
+		Rectangle r2 = new Rectangle(2.3f,3.1f,2);
 		r2.getDetails();
 
-		Rectangle r3 = new Rectangle(3);
+		Rectangle r3 = new Rectangle(1f,2f,3);
 		r3.getDetails();
 	}
 }
-
